6b074e895a9cb1cb1c1a0cbd4e2dc71fcaa6e693
Bruno Wang-Domingo <bwangdomingo@gmail.com>
2017-12-23 19:16:46 +0100

    Update get_next_line.h

--- Repository/get_next_line.h
+++ Repository/get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bwang-do <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/25 11:25:17 by bwang-do          #+#    #+#             */
-/*   Updated: 2017/12/22 16:48:40 by bwang-do         ###   ########.fr       */
+/*   Created: 2017/12/23 12:12:46 by bwang-do          #+#    #+#             */
+/*   Updated: 2017/12/23 18:14:03 by bwang-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,17 @@
 typedef struct		s_gnl
 {
 	int				fd;
-	char			*str;
-	int				len;
-	int				i;
+	char			buff[BUFF_SIZE + 1];
 	struct s_gnl	*next;
 }					t_gnl;
 
 int					get_next_line(const int fd, char **line);
-char				*ft_strdup(const char *src);
+void				ft_bzero(void *str, size_t n);
 size_t				ft_strlen(const char *str);
+char				*ft_strdup(const char *src);
 char				*ft_strcpy(char *dest, const char *src);
 char				*ft_strncpy(char *dest, const char *src, size_t n);
+char				*ft_strncat(char *dest, const char *src, size_t nb);
+int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strnew(size_t size);
 #endif
